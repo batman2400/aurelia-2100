58a938e90263bbb8dad80c65419445e79126667b
AsinthaK <asinthasenethmal@gmail.com>
2026-09-20 23:09:59 +0530

    bug fixed 2

--- src/App.tsx
+++ src/App.tsx
@@ -127,9 +127,9 @@ export const App: React.FC = () => {
           {screen === 'home' && (
             <motion.div
               key="home"
-              initial={{ opacity: 0, y: 16, filter: 'blur(3px)' }}
-              animate={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
-              exit={{ opacity: 0, y: -12, filter: 'blur(3px)' }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               transition={{ duration: 0.32, ease: [0.22, 1, 0.36, 1] }}
             >
               <HomeScreen
